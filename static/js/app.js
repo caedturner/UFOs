f70ab5bc968a668ce940f18e521b91e08d881a96
Christian Caed Turner <caed@Christians-MacBook-Pro.local>
2022-08-25 15:19:16 -0700

    Update to JavaScript.

--- static/js/app.js
+++ static/js/app.js
@@ -23,3 +23,27 @@ function buildTable(data) {
     );
     });
 }
+// crate a function that filters the data 
+function handleClick() {
+    
+    // Grab the Value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+
+    // check to see if a date was entered and filter the data using that date
+    if (date) { 
+    // apply `filter` to the table data to only keep the 
+    //rows where the ` datetime` value matches the filter value    
+    filteredData = filteredData.filter(row => row.datetime === date);
+    };
+    //rebuild the table using the filtered data
+    //@NOTE: If no date was entered, then filteredData will 
+    // be origional tableData
+    buildTable(filteredData)
+};
+
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build table when the page loads
+buildTable(tableData);
